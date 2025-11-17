58759cf88ac875fb30ff56b3024ca2988fa359ca
keianrpo-ops <keianrpo@gmail.com>
2025-11-17 13:36:35 -0500

    Update googleSheetsService.ts

--- services/services/googleSheetsService.ts
+++ services/services/googleSheetsService.ts
@@ -2,7 +2,7 @@
 
 // 👇 URL del deployment de tu Apps Script (ya con tu ID correcto)
 const API_BASE =
-  "https://script.google.com/macros/s/AKfycbweRehFO_II0a_ApKAfrs2sJm0JuKETTcgtgxlBDaYj31jjzuZmHWCDYq7WcnQ8xGns/exec";
+  "https://script.google.com/macros/s/AKfycbxPeiRiP3WQTt16qjllDW_jZo08FI53SlloiAKWcBWZPIe5qeu7wqYLXG7SYjANKj6u/exec";
 
 // 👇 NOMBRES EXACTOS de las pestañas en tu archivo de Google Sheets
 export const SHEET_EMPLOYEES   = "Empleados";
