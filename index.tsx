24e9eaf963d6c12c77d99607ebdae66e4500d7d4
keianrpo-ops <keianrpo@gmail.com>
2025-11-17 21:34:48 -0500

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -3,6 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles.css';   // ⬅️ AÑADE ESTA LÍNEA
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
